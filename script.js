26034ab460d3b7bab0be4cc8b99fcfdaed8348f1
gabriel9216 <145835885+gabriel9216@users.noreply.github.com>
2024-01-31 09:29:52 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,11 @@
-function palindrom(){
-    let word=document.getElementById("word").value;
-    word_to_check=word.toLowerCase();
-    let length_word=word_to_check.length;
-    let sprawdz=true;
-    if (length_word===0)                
-        alert("Nic nie wpisałeś!");   
-        else {
-    for (var i=0; i<length_word; i++ ) 
-        {
-        if (word_to_check.charAt(i)!=word_to_check.charAt(length_word-i-1))  
-            sprawdz=false;
-        }
-            
-        if (sprawdz==true)  
-            alert(word+ " jest palindromem."); 
-        else 
-            alert(word+" nie jest palindromem."); 
-        }
-        }
-
+const submitEvent = document.getElementById("submitEvent");
+submitEvent.addEventListener("submit", function palindrom(){ 
+    const word=document.getElementById("word").value;
+    wordToCheck=word.toLowerCase();
+    if (wordToCheck === wordToCheck.split("").reverse().join("")) {
+        alert(word+" jest palindromem");
+    }
+    else {
+        alert(word+" nie jest palindromem."); 
+    }
+    });
